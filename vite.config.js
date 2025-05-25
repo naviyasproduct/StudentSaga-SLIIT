0c03913008784e269dc42c9ef55ac79dc41d1385
Akash Naveen <naveensaranga1212@gmail.com>
2025-05-25 12:33:05 +0530

    fixes white screen 4th time

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', // relative path
-  build: {
-    outDir: 'docs',
-  },
+  base: '/StudentSaga-SLIIT/',
+  plugins: []
 });
